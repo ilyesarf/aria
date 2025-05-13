e0ac02bc82dde9d1f7ef53b448f328424435ea2b
ilyesarf <bam.boy2020@gmail.com>
2025-05-13 13:03:03 +0100

    fixed condition for rendering enigme

--- game/src/main.c
+++ game/src/main.c
@@ -158,7 +158,7 @@ int main(int argc, char** argv) {
             SDL_Delay(200); // Add a small delay to prevent immediate state toggle
         }
 
-        if (player.pos.x >= SCREEN_WIDTH){
+        if (player.pos.x >= SCREEN_WIDTH*3){
             menuState = MENU_ENIGME;
         } 
 
